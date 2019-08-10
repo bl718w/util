d14a7acfec7afc96437d90e2ab87b1400c0ccb53
xnlien <nghi.lien@wbconsultant.com>
2019-08-10 19:40:47 -0400

    update master

--- version.sh
+++ version.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
-branch=$(git branch | grep \* | cut -d ' ' -f2)
-if [ $branch == 'develop' ]; then
-   echo 'source ../environment_versions.txt'
-   source ../environment_versions.txt
+echo 'source ../environment_versions.txt'
+source ../environment_versions.txt
 
-   echo 'git checkout develop'
-   git checkout develop
+echo 'git checkout develop'
+git checkout develop
 
-   echo 'git stash'
-   git stash
+echo 'git stash'
+git stash
 
-   echo 'git pull'
-   git pull
+echo 'git pull'
+git pull
 
-   echo 'increment the version'
-   NEW_PATCH=PROD_PATCH
-   ((NEW_PATCH++))
-   echo PROD_PATCH=$PROD_PATCH NEW_PATCH=$NEW_PATCH
-   sed -i -e "s/PROD_PATCH=${PROD_PATCH}/PROD_PATCH=${NEW_PATCH}/g" ../environment_versions.txt
-   sed -i -e "s/PROD=$PROD_MAJOR.$PROD_MINOR.$PROD_PATCH/PROD\=$PROD_MAJOR.$PROD_MINOR.$NEW_PATCH/g" ../environment_versions.txt
-   source ../environment_versions.txt
+echo 'increment the version'
+NEW_PATCH=PROD_PATCH
+((NEW_PATCH++))
+echo PROD_PATCH=$PROD_PATCH NEW_PATCH=$NEW_PATCH
+sed -i -e "s/PROD_PATCH=${PROD_PATCH}/PROD_PATCH=${NEW_PATCH}/g" ../environment_versions.txt
+sed -i -e "s/PROD=$PROD_MAJOR.$PROD_MINOR.$PROD_PATCH/PROD\=$PROD_MAJOR.$PROD_MINOR.$NEW_PATCH/g" ../environment_versions.txt
+source ../environment_versions.txt
 
-   echo 'git add ../environment_versions.txt'
-   git add ../environment_versions.txt
+echo 'git add ../environment_versions.txt'
+git add ../environment_versions.txt
 
-   echo 'git commit'
-   git commit -m "Bumping production version to $PROD [skip ci]"
+echo 'git commit'
+git commit -m "Bumping production version to $PROD [skip ci]"
 
-   echo 'git push'
-   git push
-fi
+echo 'git push'
+git push
