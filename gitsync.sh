d37343328b20f4d4da756fb28e6a4203165e096c
xnlien <nghi.lien@wbconsultant.com>
2019-08-26 02:10:34 -0400

    update gitsync

--- gitsync.sh
+++ gitsync.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+# setup
+mkdir -p ~/tmp/
+yes | cp environment_versions.txt /tmp/
+gitbranch=$(git branch | grep '*' | cut -d' ' -f2)
+
+# update master branch
 git pull
 git add environment_versions.txt
 git commit -m 'update dev branch'
-git push origin
+git push
+
+# update develop branch
+git checkout develop
+yes | cp /tmp/environment_versions.txt .
+git add environment_versions.txt
+git commit -m 'update environment_versions.txt'
+git push
+
+# cleanup
+yes | rm ~/tmp/environment_versions.txt
+git checkout $gitbranch
+
